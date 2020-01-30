c7e9adc03b2c9c3b45f0c7fead5e379fa33bc49f
sebastian bautista <sebastian.bautista@rappi.com>
2020-01-29 23:08:45 -0500

    change posgres

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -83,6 +83,12 @@ function turnCreateEvent(state) {
     window.location.href = '/pages/events/post'
 }
 
+function logout() {
+    setCookie("username", "", 365);
+    setCookie("token", "", 365);
+    window.location.href = '/auth/login'
+}
+
 function goToHome() {
     window.location.href = '/pages/events'
 }
@@ -90,3 +96,10 @@ function goToHome() {
 function detail(id) {
     window.location.href = '/pages/events/'+id
 }
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
