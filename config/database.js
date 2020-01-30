a65589ad5dd1c8008c3e7f48c9f4c597edd23202
sebastian bautista <sebastian.bautista@rappi.com>
2020-01-29 22:05:18 -0500

    include docker compose

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const initOptions = {/* options as documented below */};
 
 var pgp = require('pg-promise')(initOptions)
-var db = pgp('postgres://homestead:secret@localhost:54320/proyecto0')
+var db = pgp('postgres://homestead:secret@postgres:5432/proyecto0')
 
 module.exports = {
     pgClient: db
